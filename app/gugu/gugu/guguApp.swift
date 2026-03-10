0ee1c5f0aeefe75a691c612acb415a056dbdfd60
openmpy <suhwan@kakao.com>
2026-03-10 17:15:40 +0900

    feat: 앱 초기 설정

---
+++ app/gugu/gugu/guguApp.swift
@@ -0,0 +1,17 @@
+//
+//  guguApp.swift
+//  gugu
+//
+//  Created by suhwan on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct guguApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
